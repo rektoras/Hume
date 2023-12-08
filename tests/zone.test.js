9f439ca9b396f49acdbbe6ed5b7a38ea8dbb3fad
intziral <intjannie@gmail.com>
2023-12-08 15:49:47 +0200

    Try GET zones tests

--- tests/zone.test.js
+++ tests/zone.test.js
@@ -10,6 +10,13 @@ const app = require('../index.js');
  * Test ZoneService functions
  */
 
+test('Get all zones by function', async t => {
+  const result = await get_zones('test_user');
+  // t.is(result.length, 4);
+  t.is(result[0].zone_id, 16419109);
+  t.is(result[1].zone_name, 'Hallway1');
+});
+
 
 /**
  * Test device endpoints
@@ -23,3 +30,12 @@ test.before(async (t) => {
   test.after.always((t) => {
     t.context.server.close();
   });
+
+  test.serial('GET /user/{username}/zone', async (t) => {
+    const { body, statusCode } = await t.context.got('user/test_user/zone');
+    t.is(statusCode, 200);
+    // t.is(body.length, 4);
+    t.is(body[0].zone_id, 16419109);
+    t.is(body[1].zone_name, 'Hallway1');
+  });
+  
