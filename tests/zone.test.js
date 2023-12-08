7d4b6ad80126a7333af76ff121dc8339c6617d9a
intziral <intjannie@gmail.com>
2023-12-08 15:53:56 +0200

    Updated GET zones tests

--- tests/zone.test.js
+++ tests/zone.test.js
@@ -12,9 +12,11 @@ const app = require('../index.js');
 
 test('Get all zones by function', async t => {
   const result = await get_zones('test_user');
-  // t.is(result.length, 4);
+  t.is(result.length, 2);
   t.is(result[0].zone_id, 16419109);
-  t.is(result[1].zone_name, 'Hallway1');
+  t.is(result[0].zone_name, 'Hallway1');
+  t.is(result[1].zone_id, 97527035);
+  t.is(result[1].zone_name, 'Living Room');
 });
 
 
@@ -34,8 +36,10 @@ test.before(async (t) => {
   test.serial('GET /user/{username}/zone', async (t) => {
     const { body, statusCode } = await t.context.got('user/test_user/zone');
     t.is(statusCode, 200);
-    // t.is(body.length, 4);
+    t.is(body.length, 2);
     t.is(body[0].zone_id, 16419109);
-    t.is(body[1].zone_name, 'Hallway1');
+    t.is(body[0].zone_name, 'Hallway1');
+    t.is(body[1].zone_id, 97527035);
+    t.is(body[1].zone_name, 'Living Room');
   });
   
