a4b822fa9b5b3508c2fcf322ad83b25b9d515733
nastaziaf <nastasiapht@gmail.com>
2023-12-17 22:08:58 +0200

    Add tests for post user request and change user example in swagger.yaml

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,5 +3,72 @@ const got = require('got');
 const http = require('http');
 const listen = require('test-listen');
 
-// const { add_device, delete_device, get_device, get_devices, update_device } = require('../service/DeviceService');
-// const app = require('../index.js');
+ const { create_user, get_user } = require('../service/UserService');
+ const app = require('../index.js');
+
+ /**
+ * Test UserService functions
+ */
+
+ test('Add new user by function', async t => {
+    const result = await create_user('test_user', {
+      "fullname": "Nikola Tesla",
+      "email": "nikt@gmail.com1",
+      "address": "Septembriou 20",
+      "phone": "52387447",
+      "city": "Thessaloniki",
+      "username": "nikolat",
+      "password": "current"
+    });
+    t.is(result, undefined);
+  });
+
+/**
+ * Test User endpoints
+ */
+  test.before(async (t) => {
+    t.context.server = http.createServer(app);
+    t.context.prefixUrl = await listen(t.context.server);
+    t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
+  });
+  
+  test.after.always((t) => {
+    t.context.server.close();
+  });
+  
+  test.serial('POST /user', async (t) => {
+    const { statusCode } = await t.context.got.post('user', {
+      json: {
+      "fullname": "Nikola Tesla",
+      "email": "nikt@gmail.com1",
+      "address": "Septembriou 20",
+      "phone": "52387447",
+      "city": "Thessaloniki",
+      "username": "nikolat",
+      "password": "current"
+      }
+    });
+    t.is(statusCode, 200);
+    
+    try{
+      await t.context.got.post('user');
+    } catch(error){
+      t.is(error.response.statusCode, 400);
+    }
+  
+    try{
+      await t.context.got.post('user/test_user/device', {
+        json: {
+            "fullname": "Nikola Tesla",
+            "email": "nikt@gmail.com1",
+            "address": "Septembriou 20",
+            "phone": "52387447",
+            "city": "Thessaloniki",
+            "username": "nikolat",
+            "password": "current",
+        }
+      });
+    } catch(error){
+      t.is(error.response.statusCode, 400);
+    }
+  });
