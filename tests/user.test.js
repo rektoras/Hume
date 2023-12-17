acfd716578a94d5025ed4f06540c79b04caf2780
nastaziaf <nastasiapht@gmail.com>
2023-12-17 20:17:08 +0200

    create file for user endpoints

---
+++ tests/user.test.js
@@ -0,0 +1,7 @@
+const test = require('ava');
+const got = require('got');
+const http = require('http');
+const listen = require('test-listen');
+
+// const { add_device, delete_device, get_device, get_devices, update_device } = require('../service/DeviceService');
+// const app = require('../index.js');
